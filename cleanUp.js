9220da74e31a11e265f2f8125216258e7355ebb6
Faye Rosenshein <rosefaye.a@gmail.com>
2022-09-22 11:28:52 -0600

    Initial commit

--- cleanUp.js
+++ cleanUp.js
@@ -3,23 +3,25 @@
 // YOUR TASK:
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
+  // make sure the code you submit WORKS - you can run this entire file
     // using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
 function nameQuestion() {
-  console.log("Hello, what is your name?")
+  console.log("Hello, what is your name?");
+  // I added a ; at the end of the log
 }
-
-nameQuestion()
+//  I put a ; at the end of this call
+nameQuestion();
 
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
+// I add an indent for var and added a ; at the end
+  var sum = first + second + third;
 console.log(sum);
-      }
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
@@ -37,7 +39,7 @@ makeFreshPesto();
 
 
 //  EX 4:
-function average(num1, num2) 
+function average(num1, num2)
   {
 var sum = num1 + num2;
     var avg = sum / 2;
