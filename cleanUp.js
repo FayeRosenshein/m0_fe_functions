ef11621f88b2b97498759887a5933e2875ab18a1
Faye Rosenshein <rosefaye.a@gmail.com>
2022-09-22 12:29:21 -0600

    Initial commit

--- cleanUp.js
+++ cleanUp.js
@@ -18,9 +18,9 @@ nameQuestion();
 
 // EX 2:
 function addThreeNums(first, second, third) {
-// I add an indent for var and added a ; at the end
+// I add an indent for var and console.log and added a ; at the end
   var sum = first + second + third;
-console.log(sum);
+  console.log(sum);
 }
 
 addThreeNums(1, 2, 3);
@@ -28,21 +28,27 @@ addThreeNums(4, 2, 7);
 
 
 // EX 3:
-func makeFreshPesto(){
+// I changed func to function as func is not a command and then I added a space between
+//the parenheses and the curly bracket
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");
+  // I placed the last curly bracket down one line
+}
 
 makeFreshPesto();
 
 
 //  EX 4:
-function average(num1, num2)
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
-
-  console.log(`the average is: ${avg}`.)
-  }
+// I placed the curly bracket back to the first line then I indented the first var and removed
+// an indent from the second var
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
+// I took away the last . before the parenheses and took an indent away from the last curly bracket
+// and added a final ; at the end of the console.log
+  console.log(`the average is: ${avg}`);
+}
--- functions.js
+++ functions.js
@@ -1,22 +1,60 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
 
+function printGreeting () {
+  console.log("Hello, it's nice to meet you.");
+}
+
+printGreeting();
+printGreeting();
+printGreeting();
+
+
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 
+function printGreeting2 (name) {
+  console.log(`Hello ${name}, it's nice to me you.`);
+}
+
+printGreeting2("Faye");
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 
+function company (name, payMax, payMin) {
+ var payRange = payMax - payMin;
+  console.log(`The name of this company is ${name} and the pay range is $${payRange} with a starting salary of $${payMin}`);
+}
+company("Turing","100000","80000");
 
 // 4: Write a function that satifies the following interaction pattern:
 
+function checkStock(amount, food) {
+ if (amount > 3) {
+  console.log(`${food} is stocked`);
+}
+  else if (amount < 1) {
+  console.log(`${food} - OUT of stock!`);
+}
+  else {
+  console.log(`${food} - running LOW`);
+}
+}
+
 checkStock(4, "Coffee");
-// => "Coffee is stocked"
-
 checkStock(3, "Tortillas");
-// => "Tortillas - running LOW"
-
 checkStock(0, "Cheese");
-// => "Cheese - OUT of stock!"
-
 checkStock(1, "Salsa");
-// => "Salsa - running LOW"
+
+
+
+// checkStock(4, "Coffee");
+// // => "Coffee is stocked"
+//
+// checkStock(3, "Tortillas");
+// // => "Tortillas - running LOW"
+//
+// checkStock(0, "Cheese");
+// // => "Cheese - OUT of stock!"
+//
+// checkStock(1, "Salsa");
+// // => "Salsa - running LOW"
